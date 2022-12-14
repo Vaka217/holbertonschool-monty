18207b692b36d861fce147549bd51660d1711ede
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-12-14 09:03:14 -0800

    task 3

--- 2-op_functions.c
+++ 2-op_functions.c
@@ -2,14 +2,16 @@
 
 void op_swap(stack_t **stack, __attribute__((unused)) unsigned int index)
 {
-	stack_t *swap;
+	int cp;
+
+	while ((*stack)->next)
+		(*stack) = (*stack)->next;
 	if (!*stack || !(*stack)->prev)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n", 1);
 		exit(EXIT_FAILURE);
 	}
-	swap = *stack;
+	cp = (*stack)->n;
 	(*stack)->n = (*stack)->prev->n;
-	(*stack)->prev->n = swap->n;
-	free(swap);
+	(*stack)->prev->n = cp;
 }
